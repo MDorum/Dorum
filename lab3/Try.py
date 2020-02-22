4b4365fec23e2c84edfed0891eeb56f1754cc3d2
Mikhail Dorum <dorummisha@gmail.com>
2020-02-22 22:33:31 +0100

    Added new example files

---
+++ lab3/Try.py
@@ -0,0 +1,17 @@
+from graph import *
+windowSize(500, 500)
+penColor("black")
+brushColor("yellow")
+circle(250, 250, 100) #face
+brushColor("black")
+rectangle(200, 295, 300, 275) #mouse
+brushColor("red")
+circle(210, 225, 20) #left eay
+brushColor("black")
+circle(210, 225, 7.5) #zra4ok left eay's
+brushColor("red")
+circle(300, 225, 15) #right eay
+brushColor("black")
+circle(300, 225, 7.5) #zra4ok right eay's
+
+run()
---
+++ lab3/check.py
@@ -0,0 +1,5 @@
+from graph import *
+moveTo(100,100)
+circle(0,0, 10)
+
+run()
